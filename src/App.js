9d4ee946f488f37c75de30adc44e5e0f74b40998
Dhiraj-JC <dhiraj@trainitsolutions.com>
2023-01-08 23:53:50 +0530

    feat: create components, installed dependency, register routing, login functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import CreateProduct from './Components/CreateProduct';
+import Dashboard from './Components/Dashboard';
+import DashboardContent from './Components/DashboardContent';
+import Login from './Components/Login';
+import NotFound from './Components/NotFound';
+import Products from './Components/Products';
+import SignUp from './Components/SignUp';
+import UpdateProduct from './Components/UpdateProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Navigate to='/dashboard' />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+
+        <Route path='/dashboard' element={<Dashboard />}>
+          <Route path='' element={<DashboardContent />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<UpdateProduct />} />
+          <Route path='products/new' element={<CreateProduct />} />
+        </Route>
+
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
